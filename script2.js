58ffd61e0528efcfd5c62c5957dbadff5d616608
nicolaszp <niolaszp@gmail.com>
2023-11-24 19:28:41 -0600

    Completada funcionalidad basica

--- script2.js
+++ script2.js
@@ -1,42 +1,76 @@
 const ground = document.getElementById('tablero')
-const combinaciones = [[0, 1, 2],[0, 0, 0],[1, 1, 1],[2, 2, 2],[2, 1, 0]]
-//const combinaciones = [[0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6],[1, 4, 7],[2, 5, 8],[0, 4, 8],[2, 4, 6]]
-const registro = ['','','','','','','','','']
+const combinaciones = [[0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6],[1, 4, 7],[2, 5, 8],[0, 4, 8],[2, 4, 6]]
+let registro = ['','','','','','','','','']
 let celdas = document.querySelectorAll('.celda')
 let contador = 0
-celdas.forEach(celda => {
-    celda.addEventListener('click', detectarCelda);
-  });
+celdas.forEach(celda => {celda.addEventListener('click', detectarCelda)})
+
+function reinicio(){
+    hayganador = false
+    contador = 0
+    celdas.forEach(celda => {
+        
+        while (celda.firstChild) {
+            celda.removeChild(celda.firstChild);
+        }
+    })
+    registro.fill('')
+
+}
 
 function detectarCelda(event){
     contador++
     const celdaClickeada = event.target
-    console.log(celdaClickeada)
-    let x = document.createElement('img')
     if(contador%2 == 0){
-        x.src = "kan.png"
+        const indiceColumna = Array.from(celdaClickeada.parentNode.children).indexOf(celdaClickeada)
+        if(registro[indiceColumna] == ''){
+            let x = document.createElement('img')
+            x.src = "kan.png"
+            registro[indiceColumna] = 'X'
+            celdaClickeada.appendChild(x)
+        }else{
+            alert('Casilla ocupada')
+            if(contador > 0){
+                contador--
+            }
+        }
+       
     }
     else{
-        x.src = "linux.png"
+        const indiceColumna = Array.from(celdaClickeada.parentNode.children).indexOf(celdaClickeada)
+        if(registro[indiceColumna] == ''){
+            let x = document.createElement('img')
+            x.src = "linux.png"
+            celdaClickeada.appendChild(x)
+            registro[indiceColumna] = 'O'
+        }else{
+            alert('Casilla ocupada')
+            if(contador > 0){
+                contador--
+            }
+        }
+        
     }
-    celdaClickeada.appendChild(x)
-    const indiceCelda = Array.from(celdaClickeada.parentNode.children).indexOf(celdaClickeada)
-    console.log(indiceCelda)
+    setTimeout(() => {validarGanador()}, 300);
     
 }  
 
 function validarGanador(){
-    
-}
-//window.addEventListener('load', setTablero)
-
-/*function setTablero(){
-    for(let i = 0; i < 9; i++ ){
-        let casilla = document.createElement('div')
-        casilla.classList.add('casilla')
-        casilla.id = i.toString()
-        ground.appendChild(casilla)
+    let hayGanador = false
+    for(let i = 0; i < combinaciones.length; i++){
+        const ganadora = combinaciones[i]
+        let posicion1 = registro[ganadora[0]]
+        let posicion2 = registro[ganadora[1]]
+        let posicion3 = registro[ganadora[2]]
+    if(posicion1 != '' && posicion1 == posicion2 && posicion2 == posicion3){
+        hayGanador = true;
+        alert('HAY GANADOR')
+        reinicio()
+    }    
         
+    }if(contador == 9){
+        alert('HAY EMPATE')
+        reinicio()
     }
-}*/
+}
 
